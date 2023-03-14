4083d4cdd1c13cfba2030d424fb7caa30c0c4af9
Antonio Di Novi <antonio.dinovi@simultech.it>
2023-03-14 15:08:35 +0100

    fix: update hooks

--- packages/printer-lib/src/core/usePrintData.ts
+++ packages/printer-lib/src/core/usePrintData.ts
@@ -29,3 +29,25 @@ export function usePrintData<T extends Config, K extends keyof T["schemas"]>(
 
   return { data, print };
 }
+
+export function usePrintHelper<T extends Config, K extends keyof T["schemas"]>(
+  config: T,
+  key: K
+) {
+  type dataType = z.infer<T["schemas"][K]["schema"]>;
+
+  const print = async (data: dataType) => {
+    const response = await fetch(`/api/pdf?template_id=${String(key)}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const file = await response.blob();
+    const fileURL = URL.createObjectURL(file);
+    const pdfWindow = window.open();
+    if (pdfWindow) pdfWindow.location.href = fileURL;
+  };
+
+  return { print };
+}
