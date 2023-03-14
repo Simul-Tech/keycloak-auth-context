ef45f0a6f8ef4fdbde6cd2ccdf310f7aaa7d0ed8
Antonio Di Novi <antonio.dinovi@simultech.it>
2023-03-14 14:50:14 +0100

    fix: add print helper

--- packages/printer-lib/src/core/usePrintData.ts
+++ packages/printer-lib/src/core/usePrintData.ts
@@ -3,14 +3,29 @@ import { z } from "zod";
 import { Config } from "./schemas";
 
 export function usePrintData<T extends Config, K extends keyof T["schemas"]>(
-  config?: T,
-  key?: K
+  config: T,
+  key: K
 ) {
-  const [data, setData] = useState<z.infer<T["schemas"][K]["schema"]>>();
+  type dataType = z.infer<T["schemas"][K]["schema"]>;
+
+  const [data, setData] = useState<dataType>();
 
   useEffect(() => {
     setData(window.PDF_DATA);
   }, []);
 
-  return data;
+  const print = async (data: dataType) => {
+    const response = await fetch(`/api/pdf?template_id=${String(key)}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    const file = await response.blob();
+    const fileURL = URL.createObjectURL(file);
+    const pdfWindow = window.open();
+    if (pdfWindow) pdfWindow.location.href = fileURL;
+  };
+
+  return { data, print };
 }
